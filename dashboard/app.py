0ea4038733314c1b8525c7ce9ffba67516c460df
EmovaGroup <dsi@emova-group.com>
2025-10-10 16:39:17 +0200

    Modification des 3 dernier graphique avec les trois dernière semaine plus moyenne

--- dashboard/app.py
+++ dashboard/app.py
@@ -639,6 +639,7 @@ else:
     tickets_chart = pd.concat([tickets_sel, moy_tickets], ignore_index=True)
 
     tickets_chart["jour"] = pd.Categorical(tickets_chart["jour"], categories=JOURS, ordered=True)
+    tickets_chart = tickets_chart.sort_values(by="jour")  # 🔑 ordre correct
 
     st.markdown("### 📈 Évolution des Tickets par semaine (3 dernières + Moyenne)")
 
@@ -686,6 +687,7 @@ else:
     ca_chart_df = pd.concat([ca_sel, moy_ca], ignore_index=True)
 
     ca_chart_df["jour"] = pd.Categorical(ca_chart_df["jour"], categories=JOURS, ordered=True)
+    ca_chart_df = ca_chart_df.sort_values(by="jour")  # 🔑 ordre correct
 
     st.markdown("### 📈 Évolution du CA TTC par semaine (3 dernières + Moyenne)")
 
@@ -730,6 +732,7 @@ else:
     panier_chart_df = pd.concat([pm_sel[["semaine","jour","panier_moyen"]], moy_pm], ignore_index=True)
 
     panier_chart_df["jour"] = pd.Categorical(panier_chart_df["jour"], categories=JOURS, ordered=True)
+    panier_chart_df = panier_chart_df.sort_values(by="jour")  # 🔑 ordre correct
 
     st.markdown("### 📈 Évolution du Panier moyen par semaine (3 dernières + Moyenne)")
 
